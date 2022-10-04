5ae1f65778b296152499144f467666114fcb01ea
gema20hd <gemacostzhd@hotmail.com>
2022-10-03 21:12:42 -0500

    proyecto base

---
+++ psicodidact-agendamiento-citas-front/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login/login.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { BodyComponent } from './body/body/body.component';
+import { HeaderComponent } from './header/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    BodyComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
